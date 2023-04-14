7739382f3eb21c42d025f0b6dd2a94a627b0f543
Quentin <quentin.dubois@etu.univ-cotedazur.fr>
2023-04-14 10:53:36 +0200

    algo min-max fonctionnel

--- minMaxAlgo.py
+++ minMaxAlgo.py
@@ -18,12 +18,12 @@ def play_move(board):
     depth = 3
     position_to_play = min_max_init(board, depth)
     # Because column start to 1 and not 0 in the API
-    return position_to_play[1] + 1
+    return position_to_play[0] + 1
 
 
 def min_max_init(board, depth):
-    alpha = math.inf
-    beta = -math.inf
+    alpha = -math.inf
+    beta = math.inf
     best_score = -math.inf
     best_move = None
 
@@ -31,7 +31,9 @@ def min_max_init(board, depth):
         board_copy = array_copy(board)
         board_copy[move[1]] = replace_str_index(board_copy[move[1]], move[0], MACHIN_PLAYER_REPRESENTATION)
         score = min_max(board_copy, depth - 1, False, alpha, beta)
+        # print("Score: " + str(score) + " Move: " + str(move))
         if score > best_score:
+            # print("Better Score: " + str(score) + " Move: " + str(move))
             best_score = score
             best_move = move
 
@@ -39,53 +41,46 @@ def min_max_init(board, depth):
         if beta <= alpha:
             break
 
+    # print("Best score: " + str(best_score) + " Best move: " + str(best_move))
     return best_move
 
 
 def min_max(board, depth, is_maximizing_player, alpha, beta):
-    print("Enter in minmax depth ", depth)
     if is_game_over(board) or depth == 0:
-        print("depth ", depth)
         return evaluate(board)
     else:
-        print(depth, " : depth")
         if is_maximizing_player:
             best_score = -math.inf
-            print("possible_moves(board) ", possible_moves(board))
+            print(possible_moves(board))
             for move in possible_moves(board):
                 board_copy = array_copy(board)
                 board_copy[move[1]] = replace_str_index(board_copy[move[1]], move[0], MACHIN_PLAYER_REPRESENTATION)
                 score = min_max(board_copy, depth - 1, False, alpha, beta)
-                print("score ", score)
                 best_score = max(score, best_score)
-                print("best_score ", best_score)
                 alpha = max(alpha, best_score)
                 if beta <= alpha:
                     break
-            print("best_score ", best_score)
             return best_score
         else:
             best_score = math.inf
-            print("possible_moves(board) ", possible_moves(board))
+            print(possible_moves(board))
             for move in possible_moves(board):
                 board_copy = array_copy(board)
                 board_copy[move[1]] = replace_str_index(board_copy[move[1]], move[0], HUMAN_PLAYER_REPRESENTATION)
                 score = min_max(board_copy, depth - 1, True, alpha, beta)
-                print("score ", score)
                 best_score = min(score, best_score)
                 beta = min(beta, best_score)
-                print("best_score ", best_score)
                 if beta <= alpha:
                     break
-            print("best_score ", best_score)
             return best_score
 
 def possible_moves(board):
     # return list of possible moves of the forme (column, row) or (x, y)
+    # print("Board: " + str(board))
     moves = []
     middle = 3
 
-    for row in range(HEIGHT-1, -1, -1):
+    for row in range(HEIGHT):
         if board[row][middle] == EMPTY_CELL_REPRESENTATION:
             moves.append((middle, row))
             break
@@ -94,12 +89,12 @@ def possible_moves(board):
         left = middle - i
         right = middle + i
 
-        for row in range(HEIGHT-1, -1, -1):
+        for row in range(HEIGHT):
             if board[row][left] == EMPTY_CELL_REPRESENTATION:
                 moves.append((left, row))
                 break
 
-        for row in range(HEIGHT-1, -1, -1):
+        for row in range(HEIGHT):
             if board[row][right] == EMPTY_CELL_REPRESENTATION:
                 moves.append((right, row))
                 break
